9050243457ff98099a28d062623be4630a18615a
Romashka <GrebenevRV@gmail.com>
2024-12-07 02:14:14 +0300

    add vercel

--- src/index.ts
+++ src/index.ts
@@ -6,36 +6,26 @@ import fs from "fs";
 import path from "path";
 import archiver from "archiver";
 
-// Настройка приложения
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // Порт из переменных окружения
 
+// Настройки сервера
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-// Настройка multer для загрузки нескольких файлов
-const upload = multer({
-  dest: "uploads/", // Временная директория для загрузки
-});
-
+// Настройка загрузки файлов
+const upload = multer({ dest: "uploads/" });
 const watermarkPath = path.resolve("assets", "watermark.png");
 const processedDir = "processed";
 
-// Убедимся, что директория для сохранения обработанных файлов существует
 if (!fs.existsSync(processedDir)) {
   fs.mkdirSync(processedDir);
 }
 
-// Обработчик загрузки и обработки нескольких изображений
+// Обработчик для загрузки и обработки изображений
 app.post("/process-images", upload.array("images", 10), async (req, res) => {
   try {
     const files = req.files as Express.Multer.File[];
-
-    if (!files || files.length === 0) {
-      res.status(400).send("Файлы не были загружены.");
-      return;
-    }
-
     const processedFiles: string[] = [];
 
     for (let file of files) {
@@ -44,7 +34,6 @@ app.post("/process-images", upload.array("images", 10), async (req, res) => {
 
       const metadata = await sharp(inputPath).metadata();
       const imageHeight = metadata.height || 0;
-      const imageWidth = metadata.width || 0;
 
       await sharp(inputPath)
         .composite([
@@ -60,16 +49,10 @@ app.post("/process-images", upload.array("images", 10), async (req, res) => {
       processedFiles.push(outputPath);
     }
 
-    // Создаем архив
-    const archive = archiver('zip', {
-      zlib: { level: 9 } // Максимальная степень сжатия
-    });
-
-    res.attachment('processed-images.zip'); // Устанавливаем имя архива
-
+    const archive = archiver("zip", { zlib: { level: 9 } });
+    res.attachment("processed-images.zip");
     archive.pipe(res);
 
-    // Добавляем обработанные файлы в архив
     for (let file of processedFiles) {
       archive.file(file, { name: path.basename(file) });
     }
@@ -77,11 +60,11 @@ app.post("/process-images", upload.array("images", 10), async (req, res) => {
     archive.finalize();
   } catch (error) {
     console.error(error);
-    res.status(500).send("Ошибка при обработке изображений.");
+    res.status(500).send("Ошибка обработки");
   }
 });
 
 // Запуск сервера
 app.listen(port, () => {
-  console.log(`Сервер запущен на http://localhost:${port}`);
+  console.log(`Сервер запущен на порту ${port}`);
 });
